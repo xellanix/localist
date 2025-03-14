ada44a713fbbdb0758cb22cf93f821a42dc1f728
xellanix <107340353+xellanix@users.noreply.github.com>
2025-03-14 11:08:03 +0700

    Initialize T3-App

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import "~/styles/globals.css";
+
+import { GeistSans } from "geist/font/sans";
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "LocaList",
+    description:
+        "Manage your to-do list locally in the browser across your devices.",
+    icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <html lang="en" className={`${GeistSans.variable}`}>
+            <body>{children}</body>
+        </html>
+    );
+}
